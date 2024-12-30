9047901b170c72f62484813627d98e4d60a90180
Smartisek <domalipdominik@gmail.com>
2024-12-30 18:26:24 +0000

    ✏️ Commenting Movement logic

--- Assets/My Assets/Scripts/PlayerController.cs
+++ Assets/My Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    /** Click point logic based on Pogle, accessable at: https://www.youtube.com/watch?v=LVu3_IVCzys **/
+
+    #region Variables
     const string IS_WALKING = "isWalking";
 
     PlayerInput playerInput;
@@ -19,36 +22,41 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float movementThreshold = 0.1f; // Threshold velocity to consider as moving
 
     public bool IsMoving { get; private set; } // Public property to expose movement status
+    #endregion
 
     private void Awake()
     {
-        playerInput = new PlayerInput();
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
-        AssignInputs();
+        playerInput = new PlayerInput(); //access the PlayerInput class
+        agent = GetComponent<NavMeshAgent>(); // Get NavMeshAgent component
+        animator = GetComponent<Animator>(); // Get Animator component
+        AssignInputs(); // Assign inputs
     }
 
     private void Update()
     {
+        //check if the player is moving and if the player is moving, rotate the player to the direction of movement
         if (agent.velocity.sqrMagnitude > movementThreshold && agent.remainingDistance > rotationThreshold)
         {
             FaceTarget();
         }
-        SetAnimations();
-        IsMoving = agent.velocity.sqrMagnitude > movementThreshold; // Update movement status
+        SetAnimations(); //correct animations to the actions 
+        IsMoving = agent.velocity.sqrMagnitude > movementThreshold; // Update movement status for camera rotate check 
     }
 
+    #region Movement Methods
     private void AssignInputs()
     {
-        playerInput.Main.Movement.performed += ctx => ClickMove();
+        playerInput.Main.Movement.performed += ctx => ClickMove(); //subscribe to the movement performed event created inside the input system in unity
     }
 
     private void ClickMove()
     {
-        RaycastHit hit;
+        RaycastHit hit; // Create a raycast hit variable
+        //check if the player clicks on the ground and if the player clicks on the ground, move the player to the clicked position
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickLayer))
         {
-            agent.destination = hit.point;
+            agent.destination = hit.point; // Set the destination of the NavMeshAgent to the clicked position 
+            //create an effect where we clicked 
             if (clickEffect != null)
             {
                 Instantiate(clickEffect, hit.point + new Vector3(0, 0.1f, 0), clickEffect.transform.rotation);
@@ -56,6 +64,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void FaceTarget()
+    {
+        if (agent.velocity != Vector3.zero) //if player is not just standing still
+        {
+            Vector3 direction = (agent.destination - transform.position).normalized; // Get the direction to the destination
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z)); // Get the rotation to look at the destination
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); // Rotate the player to the destination
+        }
+        else
+        {
+            return; //if player is standing still, return
+        }
+    }
+    #endregion
+
+    private void SetAnimations()
+    {
+        bool isWalking = agent.velocity.sqrMagnitude > movementThreshold; //when velocity is greater then the player is moving
+        animator.SetBool(IS_WALKING, isWalking); //set the walking animation to the player
+    }
+
+    #region Subscribers
     private void OnEnable()
     {
         playerInput.Enable();
@@ -65,23 +95,6 @@ public class PlayerController : MonoBehaviour
     {
         playerInput.Disable();
     }
+    #endregion
 
-    private void FaceTarget()
-    {
-        if (agent.velocity != Vector3.zero)
-        {
-            Vector3 direction = (agent.destination - transform.position).normalized;
-            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-        } else
-        {
-            return;
-        }
-    }
-
-    private void SetAnimations()
-    {
-        bool isWalking = agent.velocity.sqrMagnitude > movementThreshold;
-        animator.SetBool(IS_WALKING, isWalking);
-    }
 }
